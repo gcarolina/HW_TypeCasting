b8dd1315fd975e27053f5b18f1e5c0eff4b564fb
gcarolina <karolinaganizoda@gmail.com>
2022-10-30 23:53:37 +0300

    Project created

---
+++ HW_TypeCasting/HW_TypeCasting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_TypeCasting
+//
+//  Created by Carolina on 30.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
